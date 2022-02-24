accd79bfb72618391e218ec1bb33e860d7a675a6
Gonzalo <garciacalvog@hotmail.com>
2022-02-24 13:31:18 -0300

    desafio clase 3, agregado de navbar y estilos con archivo css

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <header class="boxHeather">
+            <h1 class="titulo">Aventura Wear</h1>
+            <nav class="barraNav">
+                <a href="#">Nosotros</a>
+                <a href="#">Carrito</a>
+                <a href="#">Contacto</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
