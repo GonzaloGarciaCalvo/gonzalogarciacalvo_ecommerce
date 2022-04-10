45c14eb471d9163bb14c0d41f38e0d23d58fd723
Gonzalo <garciacalvog@hotmail.com>
2022-04-10 16:20:02 -0300

    borro import no usado

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,6 +1,5 @@
 import {useState} from 'react'
 import Button from 'react-bootstrap/Button'
-
 const ItemCount  = ({stock, initial, onAdd}) => {
     
     const [contador, setContador] = useState(initial)
